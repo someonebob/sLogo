ef6f77584e25aea735060e3f6d5b8317b7b0a13c
Jesse Yue <someonebob156@gmail.com>
2017-02-23 00:17:40 -0500

    Added Clickable Text

--- src/view/TestMain.java
+++ src/view/TestMain.java
@@ -2,9 +2,13 @@ package view;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class TestMain extends Application{
+	private BorderPane root;
+	private InputBox console;
+	private Workspace workspace;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,12 +19,20 @@ public class TestMain extends Application{
 	public void start(Stage arg0) throws Exception {
 		// TODO Auto-generated method stub
 		Stage window = arg0;
-		InputBox console = new InputBox();
-		Scene scene = new Scene(console.display());
+		root = new BorderPane();
+		Scene scene = new Scene(root);
+
+		console = new InputBox();
+		root.setBottom(console.display());
+		
+		workspace = new Workspace();
+		root.setLeft(workspace.display());
+		
+
 		
 		window.setScene(scene);
 		window.setTitle("SLogo");
 		window.show();
 	}
-
+	
 }
--- src/view/Workspace.java
+++ src/view/Workspace.java
@@ -1,19 +1,35 @@
 package view;
 
 import javafx.scene.Node;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class Workspace implements PageView{
+	private ScrollPane scroll;
+	private VBox variables;
+	
+	public Workspace(){
+		initiateItems();
+	}
 
 	@Override
-	public void update() {
+	public void update(String instruction) {
 		// TODO Auto-generated method stub
-		
+		Text variable = new Text(instruction);
+		variables.getChildren().add(variable);
 	}
 
 	@Override
 	public Node display() {
 		// TODO Auto-generated method stub
-		return null;
+		return scroll;
+	}
+	
+	private void initiateItems(){
+		scroll = new ScrollPane();
+		variables = new VBox(10);
+		scroll.setContent(variables);
 	}
 
 }
