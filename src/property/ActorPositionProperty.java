b96aa0f9421fe6e84673d9011d84c36c7ebd1a57
Jimmy Shackford <jas199@duke.edu>
2017-03-10 20:06:21 -0500

    updated hover property for current turtles, added working set position buttons.

--- src/property/ActorPositionProperty.java
+++ src/property/ActorPositionProperty.java
@@ -4,15 +4,13 @@ import java.util.Arrays;
 import java.util.List;
 
 import javafx.animation.TranslateTransition;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import util.MathUtil;
 import util.PointPolar;
@@ -23,22 +21,15 @@ import view.ActorView;
  * @author jimmy
  *
  */
-public class ActorPositionProperty extends Property<Point2D> 
+public class ActorPositionProperty extends Property<Point2D>
 {
 	private ActorView actor;
-	private StringProperty position;
 
 	public ActorPositionProperty(String name, ActorView actor)
 	{
 		super(name);
 		this.actor = actor;
-		position = new SimpleStringProperty();
 		super.setValue(new Point2D(0, 0));
-		position.set(this.getValue().toString());
-	}
-	
-	public StringProperty getLocationAsString(){
-		return position;
 	}
 
 	@Override
@@ -46,7 +37,6 @@ public class ActorPositionProperty extends Property<Point2D>
 	{
 		actorMove(location);
 		super.setValue(location);
-		position.set(this.getValue().toString());
 	}
 
 	@Override
@@ -59,34 +49,40 @@ public class ActorPositionProperty extends Property<Point2D>
 			double x = Double.parseDouble(coordinates[0]);
 			double y = Double.parseDouble(coordinates[1]);
 			this.setValue(new Point2D(x, y));
-			position.set(this.getValue().toString());
 		}
 	}
 
 	@Override
 	public List<Node> makeDynamicUpdaters()
 	{
-		Label label = new Label(String.format("Move %s", this.getName()));
+		// //Label label = new Label(String.format("Move %s", this.getName()));
+		VBox xBox = new VBox();
+		VBox yBox = new VBox();
+		HBox xyBox = new HBox();
+		xBox.getChildren().add(new Label("x"));
+		yBox.getChildren().add(new Label("y"));
+		TextField x = new TextField();
+		TextField y = new TextField();
+		xBox.getChildren().add(x);
+		yBox.getChildren().add(y);
+		xyBox.setPrefWidth(100);
+		xyBox.getChildren().addAll(xBox, yBox);
 		// TODO: REPLACE THESE WITH RESOURCE FILE
-		ComboBox<String> directionPicker = new ComboBox<>(FXCollections.observableArrayList("fd", "bk", "lt", "rt"));
-		TextField distance = new TextField();
-		distance.setPromptText("Amount to move");
+		// ComboBox<String> directionPicker = new
+		// ComboBox<>(FXCollections.observableArrayList("fd", "bk", "lt",
+		// "rt"));
+		// TextField distance = new TextField();
+		// distance.setPromptText("Amount to move");
+		// Button moveButton = new Button("Move");
 		Button moveButton = new Button("Move");
 
 		moveButton.setOnAction(e -> {
-			if (directionPicker.getValue().equals("fd")) {
-				move(Double.parseDouble(distance.getText()));
-			} else if (directionPicker.getValue().equals("bk")) {
-				move(-1 * Double.parseDouble(distance.getText()));
-			} else if (directionPicker.getValue().equals("lt")) {
-				actor.setHeading(actor.getHeading() - Double.parseDouble(distance.getText()));
-			} else if (directionPicker.getValue().equals("rt")) {
-				actor.setHeading(actor.getHeading() + Double.parseDouble(distance.getText()));
-			}
+			actor.move(new Point2D(Integer.valueOf(x.getText()), Integer.valueOf(y.getText())));
 			actor.step();
 		});
-		
-		return Arrays.asList(label, directionPicker, distance, moveButton);
+
+		return Arrays.asList(xyBox, moveButton);
+		// return Arrays.asList(label, directionPicker, distance, moveButton);
 	}
 
 	private void move(double distance)
@@ -101,15 +97,14 @@ public class ActorPositionProperty extends Property<Point2D>
 
 	private void actorMove(Point2D newLocation)
 	{
-		TranslateTransition move = new TranslateTransition(Duration.millis(1000/actor.getSpeed()));
-		
-		
+		TranslateTransition move = new TranslateTransition(Duration.millis(1000 / actor.getSpeed()));
+
 		move.setFromX(this.getValue().getX());
 		move.setToX(newLocation.getX());
 		move.setFromY(this.getValue().getY());
 		move.setToY(newLocation.getY());
 		move.setCycleCount(1);
-		
+
 		actor.addTransition(move);
 	}
 
